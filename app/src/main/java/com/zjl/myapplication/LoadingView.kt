dc543f85329eacb9e49fef28de58d50f8f0977a0
Airoure <15059198089@163.com>
2021-01-14 11:38:04 +0800

    day 2

--- app/src/main/java/com/zjl/myapplication/LoadingView.kt
+++ app/src/main/java/com/zjl/myapplication/LoadingView.kt
@@ -19,31 +19,22 @@ import kotlin.math.cos
 import kotlin.math.sin
 
 
-class LoadingView :View{
-    constructor(context: Context) : this(context,null,0)
-    constructor(context: Context,attributeSet: AttributeSet):this(context,attributeSet,0)
-    constructor(context: Context,attributeSet: AttributeSet?,defStyleAttr:Int):super(context,attributeSet,defStyleAttr){
-        if(attributeSet==null)return
-        val typeArrays = context.obtainStyledAttributes(attributeSet,R.styleable.LoadingView)
-        pic = typeArrays.getDrawable(R.styleable.LoadingView_img)
-
-    }
-    private var pic:Drawable? = null
-    private val mOCPaint:Paint = Paint()
-    private val mICPaint:Paint = Paint()
-    private val mTextPaint:Paint = Paint()
-    private val mPicPaint:Paint = Paint()
-    private val mSmallTextPaint:Paint = Paint()
-    private val mArcPaint:Paint = Paint()
-    private val mColorCirclePaint:Paint = Paint()
-    private val mColorLinePaint:Paint = Paint()
-
+class LoadingView : View {
+    private var pic: Drawable? = null
+    private val mOCPaint: Paint = Paint()
+    private val mICPaint: Paint = Paint()
+    private val mTextPaint: Paint = Paint()
+    private val mPicPaint: Paint = Paint()
+    private val mSmallTextPaint: Paint = Paint()
+    private val mArcPaint: Paint = Paint()
+    private val mColorCirclePaint: Paint = Paint()
+    private val mColorLinePaint: Paint = Paint()
     private var mOuterRadius = 0f
     private var mCircleX = 0f
     private var mCircleY = 0f
     private var mInnerRadius = 0f
     private var mLineLength = 0f
-    private var process = 0
+    private var progress = 0
     private var isLoading = true
     private var alpha = 255
     private var alphaStep1 = 0
@@ -51,39 +42,56 @@ class LoadingView :View{
     private var alphaStep3 = 0
     private var rotateAngle = 0f
     private var outRoateAngle = 0f
-
-    private val arcSpeed = 2.4f
+    private val arcSpeed = 1.2f
     private val disappearSpeed = 5
     private val appearSpeed = 5
-    private val colorCircleRotate = 5
-    private val colorLineRotate = 5
-
+    private val colorCircleRotate = 0.6f
+    private val colorLineRotate = 0.6f
     private val lengthArray = FloatArray(360)
+    private val mMatrix = Matrix()
+    private var scale: Float = 0f
+    private var dx = 0
+    private var dy = 0
+    private lateinit var mBitmap: Bitmap
+    private lateinit var mShader: BitmapShader
+    private lateinit var mLinearGradient: LinearGradient
+    private lateinit var mLinearGradient2: LinearGradient
+
+    constructor(context: Context) : this(context, null, 0)
+
+    constructor(context: Context, attributeSet: AttributeSet) : this(context, attributeSet, 0)
+
+    constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attributeSet,
+        defStyleAttr
+    ) {
+        if (attributeSet == null) return
+        val typeArrays = context.obtainStyledAttributes(attributeSet, R.styleable.LoadingView)
+        pic = typeArrays.getDrawable(R.styleable.LoadingView_img)
+        typeArrays.recycle()
+    }
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        Thread{
-            var i = 0
-            while (process<100){
+        Thread {
+            while (progress < 100) {
                 mHandler.sendEmptyMessage(0)
-                i++
-                if(i %5==0){
-                    process++
-                }
                 Thread.sleep(10)
             }
             isLoading = false
-            while(true){
+            while (true) {
                 mHandler.sendEmptyMessage(0)
                 Thread.sleep(10)
             }
         }.start()
     }
 
-    private val mHandler = object : Handler(Looper.getMainLooper()){
+    private val mHandler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
-            when(msg.what){
-                0->invalidate()
+            when (msg.what) {
+                0 -> invalidate()
             }
         }
     }
@@ -92,24 +100,21 @@ class LoadingView :View{
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
-
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
         val heightSize = MeasureSpec.getSize(widthMeasureSpec)
         val mWidth = 230
         val mHeight = 230
-
-        if(widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST){
-            setMeasuredDimension(mWidth,mHeight)
+        if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(mWidth, mHeight)
         }
-        if(widthMode == MeasureSpec.AT_MOST && heightMode != MeasureSpec.AT_MOST){
-            setMeasuredDimension(mWidth,heightSize)
+        if (widthMode == MeasureSpec.AT_MOST && heightMode != MeasureSpec.AT_MOST) {
+            setMeasuredDimension(mWidth, heightSize)
         }
-        if(widthMode != MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST){
-            setMeasuredDimension(widthSize,mHeight)
-        }else{
-            setMeasuredDimension(widthSize,heightSize)
+        if (widthMode != MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSize, mHeight)
+        } else {
+            setMeasuredDimension(widthSize, heightSize)
         }
-
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
@@ -117,140 +122,180 @@ class LoadingView :View{
         initParam()
     }
 
-    private fun initParam() {
-        mOuterRadius = Math.min(width,height)*0.7.toFloat()/2
-        mCircleX = 0f
-        mCircleY = 0f
-        mInnerRadius = (mOuterRadius*0.9).toFloat()
-        mLineLength = mOuterRadius/9
-
-        for (i in 0..359){
-            lengthArray[i] = Random().nextFloat()*mLineLength*2
-        }
-    }
-
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas!!.translate((width/2).toFloat(), (height/2).toFloat())
+        canvas!!.translate((width / 2).toFloat(), (height / 2).toFloat())
         initPaint()
-        drawCircles(canvas!!)
-
-        drawLines(canvas!!)
-        if(isLoading==false){
-            hideAndShow(canvas!!)
-        }else{
-            drawColorLines(canvas!!)
-            drawText(canvas!!)
-            drawMovingArc(canvas!!)
+        drawCircles(canvas)
+        drawLines(canvas)
+        if (isLoading == false) {
+            hideAndShow(canvas)
+        } else {
+            drawColorLines(canvas)
+            drawText(canvas)
+            drawMovingArc(canvas)
         }
-
-
     }
 
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        mBitmap.recycle()
+    }
+
+    fun setProgress(progress: Int) {
+        this.progress = progress
+    }
+
+    fun setLogo(logo: Drawable) {
+        this.pic = logo
+        mBitmap = pic!!.toBitmap()
+        mShader = BitmapShader(mBitmap, Shader.TileMode.REPEAT, Shader.TileMode.CLAMP)
+    }
+
+    private fun initParam() {
+        mOuterRadius = Math.min(width, height) * 0.7.toFloat() / 2
+        mCircleX = 0f
+        mCircleY = 0f
+        mInnerRadius = (mOuterRadius * 0.9).toFloat()
+        mLineLength = mOuterRadius / 9
+        for (i in 0..359) {
+            lengthArray[i] = Random().nextFloat() * mLineLength * 2
+        }
+        mLinearGradient = LinearGradient(
+            mCircleX, mCircleY - mOuterRadius, mCircleX + mOuterRadius, mCircleY,
+            intArrayOf(
+                Color.parseColor("#001DA0FF"),
+                Color.parseColor("#2C41FB"),
+                Color.parseColor("#FF26CF")
+            ),
+            floatArrayOf(0.09f, 0.62f, 0.99f),
+            Shader.TileMode.CLAMP
+        )
+        mLinearGradient2 = LinearGradient(
+            mCircleX, mCircleY - mOuterRadius, mCircleX + mOuterRadius, mCircleY,
+            intArrayOf(
+                Color.parseColor("#1DA0FF"),
+                Color.parseColor("#2C41FB"),
+                Color.parseColor("#E547FF"),
+                Color.parseColor("#FF26CF")
+            ),
+            floatArrayOf(0.0f, 0.23f, 0.89f, 0.99f),
+            Shader.TileMode.CLAMP
+        )
+    }
 
     private fun drawMovingArc(canvas: Canvas) {
-        rotateAngle+=arcSpeed
-        canvas.rotate(rotateAngle,mCircleX,mCircleY)
-        canvas.drawArc(mCircleX-mOuterRadius,mCircleY-mOuterRadius,mCircleX+mOuterRadius,mCircleY+mOuterRadius,180f,270f,false,mArcPaint)
-
+        rotateAngle += arcSpeed
+        canvas.rotate(rotateAngle, mCircleX, mCircleY)
+        canvas.drawArc(
+            mCircleX - mOuterRadius,
+            mCircleY - mOuterRadius,
+            mCircleX + mOuterRadius,
+            mCircleY + mOuterRadius,
+            180f,
+            270f,
+            false,
+            mArcPaint
+        )
     }
 
     private fun hideAndShow(canvas: Canvas) {
-        if(alpha>0){
-            alpha-=disappearSpeed
+        if (alpha > 0) {
+            alpha -= disappearSpeed
             mTextPaint.alpha = alpha
             mSmallTextPaint.alpha = alpha
             mArcPaint.alpha = alpha
             mColorLinePaint.alpha = alpha
-
             drawColorLines(canvas)
-            canvas.drawText("${process}",mCircleX,mCircleY+50,mTextPaint)
-            canvas.drawText("%",mCircleX+mTextPaint.measureText("${process}")/2,mCircleY+50,mSmallTextPaint)
+            canvas.drawText("${progress}", mCircleX, mCircleY + 50, mTextPaint)
+            canvas.drawText(
+                "%",
+                mCircleX + mTextPaint.measureText("${progress}") / 2,
+                mCircleY + 50,
+                mSmallTextPaint
+            )
             drawMovingArc(canvas)
-        }
-        else{
-            val mBitmap = pic!!.toBitmap()
-            val mShader = BitmapShader(mBitmap,Shader.TileMode.REPEAT, Shader.TileMode.CLAMP)
-            var scale: Float
-            var dx = 0
-            var dy = 0
-            if(mBitmap.width*mOuterRadius*2>mBitmap.height*mOuterRadius*2){
-                scale = mOuterRadius*2.0f/mBitmap.width
-                dy = ((mOuterRadius*2.0f-mBitmap.height*scale)*0.5).toInt()
-            }else{
-                scale = mOuterRadius*2.0f/mBitmap.height
-                dx = ((mOuterRadius*2.0f-mBitmap.width*scale)*0.5).toInt()
+        } else {
+            if (mBitmap.width * mOuterRadius * 2 > mBitmap.height * mOuterRadius * 2) {
+                scale = mOuterRadius * 2.0f / mBitmap.width
+                dy = ((mOuterRadius * 2.0f - mBitmap.height * scale) * 0.5).toInt()
+            } else {
+                scale = mOuterRadius * 2.0f / mBitmap.height
+                dx = ((mOuterRadius * 2.0f - mBitmap.width * scale) * 0.5).toInt()
             }
-            val mMatrix = Matrix()
-            mMatrix.setScale(scale,scale)
-            mMatrix.postTranslate(dx+mCircleX-mOuterRadius,dy+mCircleY-mOuterRadius)
+            mMatrix.setScale(scale, scale)
+            mMatrix.postTranslate(dx + mCircleX - mOuterRadius, dy + mCircleY - mOuterRadius)
             mShader.setLocalMatrix(mMatrix)
-
             mPicPaint.setShader(mShader)
             mColorLinePaint.alpha = 0
             mColorCirclePaint.alpha = 0
             mPicPaint.alpha = 0
-            if(alphaStep1<255){
-                alphaStep1+=appearSpeed
+            if (alphaStep1 < 255) {
+                alphaStep1 += appearSpeed
+                alphaStep3 += appearSpeed / 2
                 mColorLinePaint.alpha = alphaStep1
-            }else if(alphaStep2<255){
-                alphaStep2+=appearSpeed
+                mPicPaint.alpha = alphaStep3
+            } else if (alphaStep2 < 255) {
+                alphaStep2 += appearSpeed
+                alphaStep3 += appearSpeed / 2
                 mColorLinePaint.alpha = 255
                 mColorCirclePaint.alpha = alphaStep2
-            }else if(alphaStep3<255){
-                alphaStep3+=appearSpeed
-                mColorLinePaint.alpha = 255
-                mColorCirclePaint.alpha = 255
                 mPicPaint.alpha = alphaStep3
-            }else{
+            } else {
                 mColorLinePaint.alpha = 255
                 mColorCirclePaint.alpha = 255
                 mPicPaint.alpha = 255
             }
             drawColorLines(canvas)
-            canvas.drawCircle(mCircleX,mCircleY,mOuterRadius,mPicPaint)
-            rotateAngle+=colorCircleRotate
-            canvas.rotate(rotateAngle,mCircleX,mCircleY)
-            canvas.drawCircle(mCircleX,mCircleY,mOuterRadius,mColorCirclePaint)
-
+            canvas.drawCircle(mCircleX, mCircleY, mOuterRadius, mPicPaint)
+            rotateAngle += colorCircleRotate
+            canvas.rotate(rotateAngle, mCircleX, mCircleY)
+            canvas.drawCircle(mCircleX, mCircleY, mOuterRadius, mColorCirclePaint)
         }
-
     }
-
 
     private fun drawText(canvas: Canvas) {
-        canvas.drawText("${process}",mCircleX,mCircleY+50,mTextPaint)
-        canvas.drawText("%",mCircleX+mTextPaint.measureText("${process}")/2,mCircleY+50,mSmallTextPaint)
-    //textsize/3
+        canvas.drawText("${progress}", mCircleX, mCircleY + 50, mTextPaint)
+        canvas.drawText(
+            "%",
+            mCircleX + mTextPaint.measureText("${progress}") / 2,
+            mCircleY + 50,
+            mSmallTextPaint
+        )
     }
 
-    private fun drawColorLines(canvas: Canvas){
+    private fun drawColorLines(canvas: Canvas) {
         canvas.save()
-        outRoateAngle+=colorLineRotate
-        canvas.rotate(-outRoateAngle,mCircleX,mCircleY)
-        for(i in 0..359 step 2){
+        outRoateAngle += colorLineRotate
+        canvas.rotate(-outRoateAngle, mCircleX, mCircleY)
+        for (i in 0..359 step 2) {
             canvas.drawLine(
-                    ((mOuterRadius+dip2px(7))*sin(2* PI/360*i)).toFloat(),
-                    ((mOuterRadius+dip2px(7))*cos(2* PI/360*i)).toFloat(),
-                    ((lengthArray[i]+ mOuterRadius+dip2px(7))*sin(2* PI/360*i)).toFloat(),
-                    ((lengthArray[i]+ mOuterRadius+dip2px(7))* cos(2* PI/360*i)).toFloat(),
-                    mColorLinePaint
+                ((mOuterRadius + dip2px(7)) * sin(2 * PI / 360 * i)).toFloat(),
+                ((mOuterRadius + dip2px(7)) * cos(2 * PI / 360 * i)).toFloat(),
+                ((lengthArray[i] + mOuterRadius + dip2px(7)) * sin(2 * PI / 360 * i)).toFloat(),
+                ((lengthArray[i] + mOuterRadius + dip2px(7)) * cos(2 * PI / 360 * i)).toFloat(),
+                mColorLinePaint
             )
-
         }
         canvas.restore()
     }
 
-    private fun drawCircles(canvas: Canvas){
-        canvas.drawCircle(mCircleX,mCircleY,mOuterRadius,mOCPaint)
-        canvas.drawCircle(mCircleX,mCircleY,mInnerRadius,mICPaint)
+    private fun drawCircles(canvas: Canvas) {
+        canvas.drawCircle(mCircleX, mCircleY, mOuterRadius, mOCPaint)
+        canvas.drawCircle(mCircleX, mCircleY, mInnerRadius, mICPaint)
     }
-    private fun drawLines(canvas: Canvas) {
-        for(i in 0..359 step 2){
-            canvas.drawLine(mCircleX,mCircleY-mOuterRadius-dip2px(7),mCircleX,mCircleY-mOuterRadius-dip2px(14)-mLineLength,mICPaint)
-            canvas.rotate(2f,mCircleX,mCircleY)
-        }
 
+    private fun drawLines(canvas: Canvas) {
+        for (i in 0..359 step 2) {
+            canvas.drawLine(
+                mCircleX,
+                mCircleY - mOuterRadius - dip2px(7),
+                mCircleX,
+                mCircleY - mOuterRadius - dip2px(14) - mLineLength,
+                mICPaint
+            )
+            canvas.rotate(2f, mCircleX, mCircleY)
+        }
     }
 
     private fun initPaint() {
@@ -260,7 +305,6 @@ class LoadingView :View{
             strokeWidth = dip2px(7)
             isAntiAlias = true
         }
-
         mICPaint.apply {
             color = Color.parseColor("#282D45")
             style = Paint.Style.STROKE
@@ -280,11 +324,6 @@ class LoadingView :View{
             isAntiAlias = true
             textSize = 50f
         }
-        val mLinearGradient=LinearGradient(mCircleX,mCircleY-mOuterRadius,mCircleX+mOuterRadius,mCircleY,
-                intArrayOf(Color.parseColor("#001DA0FF"),Color.parseColor("#2C41FB"),Color.parseColor("#FF26CF")),
-                floatArrayOf(0.09f,0.62f,0.99f),
-                Shader.TileMode.CLAMP
-        )
         mArcPaint.apply {
             shader = mLinearGradient
             strokeWidth = dip2px(7)
@@ -292,11 +331,6 @@ class LoadingView :View{
             isAntiAlias = true
             strokeCap = Paint.Cap.ROUND
         }
-        val mLinearGradient2 = LinearGradient(mCircleX,mCircleY-mOuterRadius,mCircleX+mOuterRadius,mCircleY,
-                intArrayOf(Color.parseColor("#1DA0FF"),Color.parseColor("#2C41FB"),Color.parseColor("#E547FF"),Color.parseColor("#FF26CF")),
-                floatArrayOf(0.0f,0.23f,0.89f,0.99f),
-                Shader.TileMode.CLAMP
-        )
         mColorCirclePaint.apply {
             shader = mLinearGradient2
             strokeWidth = dip2px(9)
@@ -309,12 +343,10 @@ class LoadingView :View{
             style = Paint.Style.FILL
             isAntiAlias = true
         }
-
-
-
     }
-    private fun dip2px(dipValue:Int):Float{
+
+    private fun dip2px(dipValue: Int): Float {
         val scale = context.resources.displayMetrics.density
-        return dipValue*scale+0.5f
+        return dipValue * scale + 0.5f
     }
 }
--- app/src/main/java/com/zjl/myapplication/MainActivity.kt
+++ app/src/main/java/com/zjl/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.zjl.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadingView.setLogo(resources.getDrawable(R.drawable.ic_launcher_background))
+        Thread{
+            for (i in 0..100){
+                loadingView.setProgress(i)
+                Thread.sleep(50)
+            }
+        }.start()
     }
 }
